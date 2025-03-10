cc9429a35e9530a51c36f5da00d4234c00a3f3ac
EnzoBernardis2007 <enzobernardis2007@gmail.com>
2025-03-10 19:15:16 -0300

    feat(products): using get product endpoint

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,45 +1,42 @@
-import Toolbar from "../components/Toolbar";
+import { useEffect, useState } from "react"
+import Toolbar from "../components/Toolbar"
+import api from "../api/axiosConfig"
+import { useMyContext } from "../Context/Provider"
 
-const productsMockup = [
-    {
-        name: "tablet",
-        price: 100,
-        description: "a fun description"
-    },
-    {
-        name: "smartphone",
-        price: 500,
-        description: "stay connected everywhere"
-    },
-    {
-        name: "laptop",
-        price: 1200,
-        description: "powerful and portable"
-    },
-    {
-        name: "headphones",
-        price: 150,
-        description: "immersive sound experience"
-    },
-    {
-        name: "smartwatch",
-        price: 250,
-        description: "track your fitness and more"
-    },
-    {
-        name: "camera",
-        price: 800,
-        description: "capture your best moments"
-    }
-];
+interface ProductsModel {
+    id: number
+    name: string
+    description: string
+    price: number,
+    quantity: number
+    registration: string
+}
 
 export default function Product() {
+    const { addModal } = useMyContext()
+    const [products, setProducts] = useState<ProductsModel[]>([])
+
+    useEffect(() => {
+        const getAllProducts = async () => {
+            const response = await api.get("/product")
+
+            if(response.status == 200) {
+                const data = response.data
+                setProducts(data)
+            } else {
+                addModal("Failed to get products data", "error")
+            }
+        }
+
+        getAllProducts()
+    }, [products])
+
     return (
         <main className="h-full w-full flex">
             <Toolbar />
             <div className="h-[94%] w-full p-6 overflow-hidden"> {/* Container principal sem scroll */}
                 <div className="h-[96%] overflow-y-auto"> {/* Container dos produtos com scroll */}
-                    {productsMockup.map((product, index) => (
+                    {products.map((product, index) => (
                         <div key={index} className="bg-slate-200 w-full h-fit p-4 mb-4 rounded-md">
                             <p><strong>Name:</strong> {product.name}</p>
                             <p><strong>Price:</strong> ${product.price}</p>
@@ -49,5 +46,5 @@ export default function Product() {
                 </div>
             </div>
         </main>
-    );
+    )
 }
